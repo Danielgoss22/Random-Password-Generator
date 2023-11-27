a3c1f1fe8944de66b31cec734c38430226711450
danielgoss22 <daniel.goss22@gmail.com>
2023-11-27 09:28:08 -0700

    added notes and code from tutor session

--- script.js
+++ script.js
@@ -1,21 +1,6 @@
 // Assignment code here
 
 // Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-//define and array of all lower case letter etc
-
 var lowerCase = [
   "a",
   "b",
@@ -104,3 +89,90 @@ var specialCharacters = [
   "~",
 ];
 var numericCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var generateBtn = document.querySelector("#generate");
+
+function passwordOptions() {
+  var length = parseInt(
+    prompt("How many characters do you want your password to be?")
+  );
+  if (length < 8 || length > 128) {
+    alert("Number of charaters must be between 8 and 128.");
+    return null;
+  }
+  if (Number.isNaN(length)) {
+    alert("The selection must be a number between 8 and 128.");
+    return null;
+  }
+  var isLowerCase = confirm("Do you want lowercase letters in your password?");
+  var isUpperCase = confirm("Do you want uppercase letters in your password?");
+  var isSpecialCharaters = confirm(
+    "Do you want special characters in your password?"
+  );
+  var isNumericCharacters = confirm("Do you want numbers in your password?");
+  if (
+    isLowerCase === false &&
+    isUpperCase === false &&
+    isSpecialCharaters === false &&
+    isNumericCharacters === false
+  ) {
+    alert("Must choose one character type.");
+    return null;
+  }
+
+  var userInput = {
+    length: length,
+    isLowerCase: isLowerCase,
+    isUpperCase: isUpperCase,
+    isSpecialCharaters: isSpecialCharaters,
+    isNumericCharacters: isNumericCharacters,
+  };
+  return userInput;
+}
+function getRandom(arr) {
+  var index = Math.floor(Math.random() * arr.length);
+  var randomElement = arr[index];
+  return randomElement;
+}
+function generatePassword() {
+  var custInfo = passwordOptions();
+  //empty array = garuntee array, var garuntee need another array called possibilities take these and randomize against a random function then join into a final array then turn that array into a string declare 3 empty arrays
+  //take custinfo with conditions to make sure it is true
+  if (custInfo.isLowerCase) {
+    possibilities = possibilities.concat(lowerCase);
+    garuantee.push(getRandom(lowerCase));
+  }
+  if (custInfo.isLowerCase) {
+    possibilities = possibilities.concat(lowerCase);
+    garuantee.push(getRandom(lowerCase));
+  }
+  if (custInfo.isLowerCase) {
+    possibilities = possibilities.concat(lowerCase);
+    garuantee.push(getRandom(lowerCase));
+  }
+  if (custInfo.isLowerCase) {
+    possibilities = possibilities.concat(lowerCase);
+    garuantee.push(getRandom(lowerCase));
+  }
+
+  //randomize my garuantee
+  for (let i = 0; i < garuantee.length; i++) {
+    result[i] = garuantee[i];
+  }
+  for (let i = 0; i < custInfo.length; i++) {
+    var possible = getRandom[possibilities];
+    result.push(possible);
+  }
+}
+//
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+//define and array of all lower case letter etc
